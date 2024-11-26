ffe9fbc58cdceafa555e992ca91642c56cf15d49
Lily1333 <liviavitoriazavatiero@gmail.com>
2024-11-25 21:08:51 -0300

    Add files via upload

---
+++ Livia-Z-flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Maquiagem',
+    'Qual produto de maquiagem é essencial para cobrir imperfeições?',
+    'O corretivo é essencial para cobrir imperfeições e proporcionar um acabamento uniforme à pele.'
+)
+
+criaCartao(
+    'História',
+    'Quando o Brasil aboliu a escravidão?',
+    'Em 13 de maio de 1888, com a Lei Áurea'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Qual é a palavra mais longa da língua portuguesa?',
+    'Pneumoultramicroscopicossilico-\nvulcanoconiótico.'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Qual produto de maquiagem é usado para alongar e dar volume aos cílios?',
+    'Máscara para cílios (RÍMEL)'
+)
